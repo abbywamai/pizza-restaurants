056cecea698fe4cab7879c5ed3e119d3ca81b568
abigail <abigail.wamai@student.moringaschool.com>
2023-09-25 08:13:34 +0300

    added restaurants-pizza database

--- seed.py
+++ seed.py
@@ -11,39 +11,27 @@ from models import db, Restaurant, Pizza, RestaurantPizza
 fake = Faker()
 
 # Seed Restaurants
-# def seed_restaurants():
-#     for _ in range(5):
-#         restaurant = Restaurant(
-#             name=fake.company(),
-#             address=fake.address()
-#         )
-#         db.session.add(restaurant)
-#     db.session.commit()
+def seed_restaurants():
+    for _ in range(5):
+        restaurant = Restaurant(
+            name=fake.company(),
+            address=fake.address()
+        )
+        db.session.add(restaurant)
+    db.session.commit()
 
-# # Seed Pizzas
-# def seed_pizzas():
-#     pizza_data = [
-#         {"name": "Margherita", "ingredients": "Tomato, Mozzarella, Basil"},
-#         {"name": "Pepperoni", "ingredients": "Tomato, Mozzarella, Pepperoni"},
-#         {"name": "Vegetarian", "ingredients": "Tomato, Mozzarella, Vegetables"}
-#     ]
+# Seed Pizzas
+def seed_pizzas():
+    pizza_data = [
+        {"name": "Margherita", "ingredients": "Tomato, Mozzarella, Basil"},
+        {"name": "Pepperoni", "ingredients": "Tomato, Mozzarella, Pepperoni"},
+        {"name": "Vegetarian", "ingredients": "Tomato, Mozzarella, Vegetables"}
+    ]
 
-#     for pizza_info in pizza_data:
-#         pizza = Pizza(**pizza_info)
-#         db.session.add(pizza)
-#     db.session.commit()
-
-# # Seed RestaurantPizza
-# def seed_restaurant_pizzas():
-#     for restaurant in Restaurant.query.all():
-#         for pizza in Pizza.query.all():
-#             restaurant_pizza = RestaurantPizza(
-#                 price=fake.random_number(decimals=2, fix_len=True),
-#                 restaurant_id=restaurant.id,
-#                 pizza_id=pizza.id
-#             )
-#             db.session.add(restaurant_pizza)
-#     db.session.commit()
+    for pizza_info in pizza_data:
+        pizza = Pizza(**pizza_info)
+        db.session.add(pizza)
+    db.session.commit()
 
 def seed_restaurant_pizzas():
     for restaurant in Restaurant.query.all():
@@ -59,7 +47,7 @@ def seed_restaurant_pizzas():
 
 if __name__ == '__main__':
     with app.app_context():
-        # seed_restaurants()
-        # seed_pizzas()
+        seed_restaurants()
+        seed_pizzas()
         seed_restaurant_pizzas()
 
